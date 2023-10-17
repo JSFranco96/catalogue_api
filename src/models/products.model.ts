fecdde294b978d89316621dcc7bcdf1761c0e140
JSFranco96 <javierfranco0904@gmail.com>
2023-10-17 12:52:17 -0500

    feat: :sparkles: Historial de cambios en stock y price

--- src/models/products.model.ts
+++ src/models/products.model.ts
@@ -37,7 +37,7 @@ const productsSchema = new Schema(
                 date: Date
             }
         ],
-        sotckHistory: [
+        stockHistory: [
             {
                 stock: Number,
                 date: Date
@@ -53,6 +53,43 @@ const productsSchema = new Schema(
     }
 )
 
+// Con este hook, interceptamos el momento en que se vaya a actualizar el producto
+// con el método "findOneAndUpdate" para verificar si los campos "price" o "stock"
+// se están actualizando y así guardar el historial de cambios de estos.
+productsSchema.pre('findOneAndUpdate', async function () {
+    try {
+        const newPrice = this.get('price')
+        const newStock = this.get('stock')
+        const _id = this.get('_id')
+        const current = await Products.findById(_id, { price: 1, stock: 1 }).lean()
+        if (current && current.price && current.stock) {
+
+            let $push: any = {}
+
+            if (current.price !== newPrice) {
+                $push.priceHistory = {
+                    price: current.price,
+                    date: new Date()
+                }
+            }
+
+            if (current.stock !== newStock) {
+                $push.stockHistory = {
+                    stock: current.stock,
+                    date: new Date()
+                }
+            }
+
+            if ($push) {
+                await Products.updateOne({ _id }, { $push })
+            }
+        }
+    } catch (error) {
+        console.log('❌ Error en hook para actualizar historial de price y stock', error)
+    }
+})
+
 const Products = model('products', productsSchema)
+
 
 export { Products }
